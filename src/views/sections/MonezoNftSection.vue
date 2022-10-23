7ae55b603c3bfae7ff61a34955ab9fbd14bdf3cf
demiavaliani <demikoavaliani@gmail.com>
2022-10-23 19:38:37 +0300

    Remove comments

--- src/views/sections/MonezoNftSection.vue
+++ src/views/sections/MonezoNftSection.vue
@@ -54,127 +54,8 @@
 			height: 100vh;
 
 			img {
-				// width: auto;
 				width: 49vw;
-				// height: 70%;
 			}
-
-			// img {
-			// 	position: absolute;
-			// 	width: auto;
-			// 	max-height: 43.2%;
-
-			// 	&:first-child {
-			// 		top: 21.6%;
-			// 		left: 15.5%;
-			// 		// top: 18rem;
-			// 		// left: -10rem;
-			// 		max-height: 38.9%;
-			// 	}
-
-			// 	&:nth-child(2) {
-			// 		top: 11.5%;
-			// 		left: 18.4%;
-			// 		// top: 9.5rem;
-			// 		// left: -7rem;
-			// 		z-index: 1;
-			// 	}
-
-			// 	&:nth-child(3) {
-			// 		top: 33.6%;
-			// 		// left: 45.6%;
-			// 		left: 38.4%;
-			// 		// top: 28rem;
-			// 		// left: 21.5rem;
-			// 		max-height: 38.9%;
-			// 	}
-
-			// 	&:nth-child(4) {
-			// 		top: 23.6%;
-			// 		// left: 48.5%;
-			// 		left: 44.9%;
-			// 		// top: 19.6rem;
-			// 		// left: 24.5rem;
-			// 		z-index: 1;
-			// 	}
-			// }
-
-			// img {
-			// 	position: relative;
-			// 	// position: absolute;
-			// 	width: auto;
-			// 	height: 43.2%;
-
-			// 	&:first-child {
-			// 		// top: 18rem;
-			// 		// left: -10rem;
-			// 		top: 17.3%;
-			// 		left: 0.8%;
-			// 		height: 38.9%;
-			// 	}
-
-			// 	&:nth-child(2) {
-			// 		// top: 9.5rem;
-			// 		// left: -7rem;
-			// 		top: 11.5%;
-			// 		left: -20.9%;
-			// 		z-index: 1;
-			// 	}
-
-			// 	&:nth-child(3) {
-			// 		// top: 28rem;
-			// 		// left: 21.5rem;
-			// 		top: 29.3%;
-			// 		left: -22.3%;
-			// 		height: 38.9%;
-			// 	}
-
-			// 	&:nth-child(4) {
-			// 		// top: 19.6rem;
-			// 		// left: 24.5rem;
-			// 		top: -20.1%;
-			// 		left: 7.6%;
-			// 		z-index: 1;
-			// 	}
-			// }
-
-			// img {
-			// 	position: absolute;
-			// 	width: auto;
-			// 	height: 43.2%;
-
-			// 	&:first-child {
-			// 		top: 18rem;
-			// 		left: -10rem;
-			// 		top: 21.6%;
-			// 		left: 12.8%;
-			// 		height: 38.9%;
-			// 	}
-
-			// 	&:nth-child(2) {
-			// 		top: 9.5rem;
-			// 		left: -7rem;
-			// 		top: 11.5%;
-			// 		left: 15.5%;
-			// 		z-index: 1;
-			// 	}
-
-			// 	&:nth-child(3) {
-			// 		top: 28rem;
-			// 		left: 21.5rem;
-			// 		top: 33.6%;
-			// 		left: 41.3%;
-			// 		height: 38.9%;
-			// 	}
-
-			// 	&:nth-child(4) {
-			// 		top: 19.6rem;
-			// 		left: 24.5rem;
-			// 		top: 23.6%;
-			// 		left: 44%;
-			// 		z-index: 1;
-			// 	}
-			// }
 		}
 
 		&__right--description {
@@ -182,7 +63,6 @@
 			flex-direction: column;
 			align-items: flex-start;
 			justify-content: center;
-			// width: auto;
 
 			& .text {
 				margin-bottom: 3.6rem;
